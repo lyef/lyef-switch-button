246609df3a5e7fa78ed3dcbc073493961be33806
Amine Mouafik <amine@mouafik.fr>
2017-04-20 23:09:53 +0700

    Fix deprecated usage of PropType via main React package (#4)
    * Fix deprecated usage of PropType via main React package
    
    * 1.2.3

--- src/Label.js
+++ src/Label.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const Label = ({ name, id }) => (
     <label className="label" htmlFor={id}>{name}</label>
