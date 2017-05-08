ee5cc2e9728ce4c470d29a2b316375e70839ee1f
Amine Mouafik <amine@mouafik.fr>
2017-05-08 20:56:01 +0700

    Rebuild dist files (#5)
    * Rebuild dist files
    
    * 1.3.1

--- dist/Toggle.js
+++ dist/Toggle.js
@@ -2,34 +2,38 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
-var _react = require("react");
+var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+var _propTypes = require('prop-types');
+
+var _propTypes2 = _interopRequireDefault(_propTypes);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
 var Toggle = function Toggle(_ref) {
-    var id = _ref.id;
-    var isChecked = _ref.isChecked;
-    var action = _ref.action;
-    return _react2["default"].createElement(
-        "div",
-        { className: "toggle-container" },
-        _react2["default"].createElement("input", {
+    var id = _ref.id,
+        isChecked = _ref.isChecked,
+        action = _ref.action;
+    return _react2['default'].createElement(
+        'div',
+        { className: 'toggle-container' },
+        _react2['default'].createElement('input', {
             id: id,
-            className: "input",
-            type: "checkbox",
+            className: 'input',
+            type: 'checkbox',
             defaultChecked: isChecked,
             onChange: action
         }),
-        _react2["default"].createElement("label", { className: "toggle", htmlFor: id })
+        _react2['default'].createElement('label', { className: 'toggle', htmlFor: id })
     );
 };
 
 Toggle.propTypes = {
-    id: _react.PropTypes.string.isRequired,
-    isChecked: _react.PropTypes.bool.isRequired,
-    action: _react.PropTypes.func
+    id: _propTypes2['default'].string.isRequired,
+    isChecked: _propTypes2['default'].bool,
+    action: _propTypes2['default'].func
 };
 
-exports["default"] = Toggle;
+exports['default'] = Toggle;
